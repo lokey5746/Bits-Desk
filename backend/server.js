b0c5741655b0f2f9e362ee89eeaeeed503285e63
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-08-08 00:15:12 +0545

    simple server setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  5000,
+  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
