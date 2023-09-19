1c4a3097667b69f125cb5a043178c2e0eb71a022
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-09-19 15:21:00 +0545

    auth middleware added

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import Admin from "../models/admin/adminModel.js";
+import verifyToken from "../utilis/verifyToken.js";
+
+const protect = async (req, res, next) => {
+  // get token
+  const headerObj = req.headers;
+  // console.log(headerObj);
+  const token = headerObj?.authorization?.split(" ")[1];
+  // console.log(token);
+  // verify token
+  const verifiedToken = verifyToken(token);
+  if (verifiedToken) {
+    const user = await Admin.findById(verifiedToken.id).select(
+      "name email role"
+    );
+    req.userAuth = user;
+    // console.log(req.userAuth);
+    next();
+  } else {
+    const err = new Error("Token expired/invalid");
+    next(err);
+  }
+};
+
+export { protect };
