2dd99c6332ff055b05f87cd71c12b9be11e5872b
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-09-19 15:35:30 +0545

    login admin added

--- backend/controllers/admin/adminController.js
+++ backend/controllers/admin/adminController.js
@@ -1,6 +1,7 @@
 import Admin from "../../models/admin/adminModel.js";
 import asyncHandler from "express-async-handler";
 
+import generateToken from "../../utilis/generateToken.js";
 import { hashPassword, verifyPassword } from "../../utilis/helpers.js";
 
 // @desc Create  admins
@@ -28,4 +29,27 @@ const registerAdmin = asyncHandler(async (req, res) => {
   });
 });
 
-export { registerAdmin };
+// @desc Create  admins
+// @route POST /api/v1/admins/register
+// @access Public
+
+const loginAdmin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //   find the user
+  const admin = await Admin.findOne({ email });
+  if (!admin) {
+    throw new Error("admin not found");
+  }
+  //   verify password
+  const isMatched = await verifyPassword(password, admin.password);
+  if (!isMatched) {
+    throw new Error("invalid login details");
+  }
+  res.status(201).json({
+    status: "success",
+    data: generateToken(admin._id),
+    message: "admin login successfully",
+  });
+});
+
+export { registerAdmin, loginAdmin };
--- backend/routes/admin/adminRoutes.js
+++ backend/routes/admin/adminRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { registerAdmin } from "../../controllers/admin/adminController.js";
+import {
+  registerAdmin,
+  loginAdmin,
+} from "../../controllers/admin/adminController.js";
 
 const router = express.Router();
 
 router.route("/register").post(registerAdmin);
+router.route("/login").post(loginAdmin);
 
 export default router;
