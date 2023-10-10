9132cd724d945043699f1b7d598a50e94bf4bebc
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-10-10 16:48:07 +0545

    dashboard routing fix

--- frontend/src/screen/layout/DashboardLayout.jsx
+++ frontend/src/screen/layout/DashboardLayout.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default DashboardLayout;
