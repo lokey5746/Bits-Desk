a4239be89cc6f655253bdc2fbfbb125c9c90002f
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-10-10 14:41:41 +0545

    user signin & register added

--- frontend/src/features/user/general/userService.js
+++ frontend/src/features/user/general/userService.js
@@ -1,21 +1,35 @@
 import axios from "axios";
-import { json } from "express";
 
 const API_URL = "/api/v1/users/";
 
 //  register user
 
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData);
-  if (response.data) {
-    localStorage.setItem("user", JSON.stringify(response.data));
+  const res = await axios.post(API_URL, userData);
+  if (res.data) {
+    localStorage.setItem("user", JSON.stringify(res.data));
   }
 
-  return response.data;
+  return res.data;
+};
+
+const signin = async (userData) => {
+  const res = await axios.post(API_URL + "signin", userData);
+  if (res.data) {
+    localStorage.setItem("user", JSON.stringify(res.data));
+  }
+  return res.data;
+};
+
+// signout user
+const signout = async () => {
+  localStorage.removeItem("user");
 };
 
 const authService = {
   register,
+  signout,
+  signin,
 };
 
 export default authService;
--- frontend/src/features/user/general/userSlice.js
+++ frontend/src/features/user/general/userSlice.js
@@ -26,6 +26,27 @@ export const register = createAsyncThunk(
   }
 );
 
+// login
+export const signin = createAsyncThunk(
+  "auth/signin",
+  async (user, thunkAPI) => {
+    try {
+      return await userService.signin(user);
+    } catch (error) {
+      (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Logout user
+
+export const signout = createAsyncThunk("auth/signout", async () => {
+  await userService.signout();
+});
+
 export const userSlice = createSlice({
   name: "user",
   initialState,
@@ -52,6 +73,26 @@ export const userSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
         state.user = null;
+      })
+      .addCase(signin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(signin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(signin.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(signout.fulfilled, (state) => {
+        state.user = null;
       });
   },
 });
+
+export const { reset } = userSlice.actions;
+export default userSlice.reducer;
