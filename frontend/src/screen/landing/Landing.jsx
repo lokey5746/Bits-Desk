3b6bc0af6dcc4e91c93d8b9b6d947e0401353f1e
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-10-10 16:35:46 +0545

    landing page added

--- frontend/src/screen/landing/Landing.jsx
+++ frontend/src/screen/landing/Landing.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+import banner from "../../assets/banner.png";
 
 const Landing = () => {
-  return <div className="text-center text-xl my-4 font-bold">Landing page</div>;
+  return (
+    <section className="w-full mx-auto md:max-w-7xl py-5">
+      <div className="flex justify-between">
+        <img src={logo} alt="" className=" h-14" />
+        <div>
+          <ul className="text-lg font-bold">
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <img src={banner} className="h-full" alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Landing;
