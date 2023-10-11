b3dd6a14a528a8736e747f44c6e22d9c83660526
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-10-11 09:42:31 +0545

    small fix

--- frontend/src/screen/landing/Landing.jsx
+++ frontend/src/screen/landing/Landing.jsx
@@ -8,10 +8,19 @@ const Landing = () => {
     <section className="w-full mx-auto md:max-w-7xl py-5">
       <div className="flex justify-between">
         <img src={logo} alt="" className=" h-14" />
-        <div>
-          <ul className="text-lg font-bold">
+        <div className="">
+          <ul className="text-lg font-bold flex space-x-5">
             <li>
               <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/signin">Blog</Link>
+            </li>
+            <li>
+              <Link to="/signin">About Us</Link>
+            </li>
+            <li>
+              <Link to="/signin">Contact Us</Link>
             </li>
           </ul>
         </div>
